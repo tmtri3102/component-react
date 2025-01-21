36a9b76370fd2ab8b8d4ba77fb8bced5f44fadb8
tmtri3102 <tmtri310251@gmail.com>
2025-01-21 11:33:41 +0700

    componentDidMount

--- src/App.js
+++ src/App.js
@@ -20,23 +20,47 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      number: 0,
+      // number: 0,
+      color: "black",
     };
   }
 
-  increase = () => {
-    this.setState({ number: this.state.number + 1 });
-  };
-  decrease = () => {
-    this.setState({ number: this.state.number - 1 });
-  };
+  // increase = () => {
+  //   this.setState({ number: this.state.number + 1 });
+  // };
+  // decrease = () => {
+  //   this.setState({ number: this.state.number - 1 });
+  // };
+  // changeColor = () => {
+  //   this.setState({ color: "red" });
+  // };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ color: "pink" });
+    }, 5000);
+  }
 
   render() {
     return (
       <div style={{ textAlign: "center", padding: 30 }}>
-        <button onClick={this.decrease}>Decrease</button>
-        <span style={{ padding: 10 }}>{this.state.number}</span>
-        <button onClick={this.increase}>Increase</button>
+        {/* <div>
+          <button onClick={this.decrease}>Decrease</button>
+          <span style={{ padding: 10 }}>{this.state.number}</span>
+          <button onClick={this.increase}>Increase</button>
+        </div>
+        <div style={{ backgroundColor: this.state.color, padding: 80 }}>
+          <button onClick={this.changeColor}>Change color</button>
+        </div> */}
+        <div
+          style={{
+            backgroundColor: this.state.color,
+            paddingTop: 20,
+            width: 400,
+            height: 80,
+            margin: "auto",
+          }}
+        />
       </div>
     );
   }
