55d25515090fabc3027e0c6ffb4d9d5b88894a8c
tmtri3102 <tmtri310251@gmail.com>
2025-01-21 10:54:41 +0700

    first prop in react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome name="Sara" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,5 @@
+function Welcome(props) {
+  console.log(props);
+  return <h1>Welcome, {props.name}</h1>;
+}
+export default Welcome;
